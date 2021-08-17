26504df283a0447363b40d849ab6d19ca574dde8
decimalon <jyothishp994@gmail.com>
2021-08-17 08:12:56 +0530

    Updated the next palindrome date logic

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-btnSubmitVar=document.querySelector('#submitdate');
-birthdayVar=document.querySelector('#birthday');
-btnSubmitVar.addEventListener("click",checkPalindrome);
-btnSubmitVar.addEventListener("click",ShowLoader);
-function checkPalindrome()
+var found=0;
+function IsPalindrome()
 {
 
 if(birthdayVar.value){
@@ -15,7 +12,7 @@ if(birthdayVar.value){
    dateVar=tempdateVar.length>1?tempdateVar:"0"+tempdateVar;
    monthVar=tempmonthVar.length>1?tempmonthVar:"0"+tempmonthVar;
    yearVar=dt.getFullYear()+"";
-   format_date=['ddmmyyyy','yyyymmdd','mm-dd-yy','ddmmyy','mmddyyyy'];
+   //format_date=['ddmmyyyy','yyyymmdd','mmddyy','ddmmyy','mmddyyyy','yyddmm'];
    possible_comb=[dateVar+"-"+monthVar+"-"+yearVar,
        yearVar+"-"+monthVar+"-"+dateVar,
    monthVar+"-"+dateVar+"-"+yearVar.substr(-2),
@@ -50,77 +47,206 @@ rev_comb_user_date_no_hypen.forEach(rev_element => {
         if(rev_element==actual_element)
         {
             found=1;
+            console.log(found)
         }
     });
 });
-Palindrome_date_diff=[]
-Palindromes=["02-12-2021","01-02-2010","11-02-2011","02-02-2020","12-02-2021","03-02-2030","04-02-2040","05-02-2050","06-02-2060"];
-console.log(Palindromes);
-usr_req_fmt_dt=new Date(req_fmt_dt);
-var diffTime=0;
-var diffDays=0;
-Palindromes.forEach(element => {
-    console.log(element)
-    console.log(req_fmt_dt)
-    tempelement=new Date(element);
- diffTime = Math.abs(usr_req_fmt_dt - tempelement);
- diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
- Nearest_Palindrome_date=element;
- Palindrome_date_diff.push(diffDays)
-});
-console.log(Palindrome_date_diff);
-min_value=Palindrome_date_diff[0]
-min_index=0
-for(index=1;index<Palindrome_date_diff.length;index++)
-{
-if(min_value>Palindrome_date_diff[index])
-{
-    min_value=Palindrome_date_diff[index];
-    min_index=index;
 }
+return found;
 }
-console.log(min_value)
-console.log(min_index)
+function reverseString(str) {
+    var listOfChars = str.split('');
+    var reversedListOfChar = listOfChars.reverse();
+    var reversedString = reversedListOfChar.join('');
+    return reversedString;
+  }
+  
+  function isStringPalindrome(str) {
+    var reversedString = reverseString(str);
+    return str === reversedString;
+  }
+  
+  function getDateAsString(date) {
+    var dateInStr = { day: '', month: '', year: '' };
+  
+    if (date.day < 10) {
+      dateInStr.day = '0' + date.day;
+    }
+    else {
+      dateInStr.day = date.day.toString();
+    }
+  
+    if (date.month < 10) {
+      dateInStr.month = '0' + date.month;
+    }
+    else {
+      dateInStr.month = date.month.toString();
+    }
+  
+    dateInStr.year = date.year.toString();
+    return dateInStr;
+  }
+  
+  function getDateInAllFormats(date) {
+    var ddmmyyyy = date.day + date.month + date.year;
+    var mmddyyyy = date.month + date.day + date.year;
+    var yyyymmdd = date.year + date.month + date.day;
+    var ddmmyy = date.day + date.month + date.year.slice(-2);
+    var mmddyy = date.month + date.day + date.year.slice(-2);
+    var yyddmm = date.year.slice(-2) + date.day + date.month;
+  
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yyddmm];
+  }
+  
+  function checkPalindromeForAllDateFormats(date) {
+    var dateFormatList = getDateInAllFormats(date);
+    var palindromeList = [];
+  
+    for (var i = 0; i < dateFormatList.length; i++) {
+      var result = isStringPalindrome(dateFormatList[i]);
+      palindromeList.push(result);
+    }
+    return palindromeList;
+  }
+  
+  function isLeapYear(year) {
+  
+    if (year % 400 === 0)
+      return true;
+  
+    if (year % 100 === 0)
+      return false;
+  
+    if (year % 4 === 0)
+      return true;
+  
+    return false;
+  }
+  
+  function getNextDate(date) {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+  
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  
+    if (month === 2) {
+      if (isLeapYear(year)) {
+        if (day > 29) {
+          day = 1;
+          month = 3;
+        }
+      }
+      else {
+        if (day > 28) {
+          day = 1;
+          month = 3;
+        }
+      }
+    }
+    else {
+      if (day > daysInMonth[month - 1]) {
+        day = 1;
+        month++;
+      }
+    }
+  
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+  
+    return {
+      day: day,
+      month: month,
+      year: year
+    }
+  }
+  
+  
+  function getNextPalindromeDate(date) {
+  
+    var nextDate = getNextDate(date);
+    var ctr = 0;
+  
+    while (1) {
+      ctr++;
+      var dateStr = getDateAsString(nextDate);
+      var resultList = checkPalindromeForAllDateFormats(dateStr);
+  
+      for (let i = 0; i < resultList.length; i++) {
+        if (resultList[i]) {
+          return [ctr, nextDate];
+        }
+      }
+      nextDate = getNextDate(nextDate);
+    }
+  }
+  function ShowLoader()
+  {
+      loaderVar=document.querySelector(".loader")
+      setTimeout(() => { loaderVar.style.display="block";
+      document.querySelector('.display').innerHTML = ''; }, 0);
+  }
+  function getPosition(string, subString, index) {
+    return string.split(subString, index).join(subString).length;
+  }
+  
+  function hideLoader()
+  {
+      loaderVar=document.querySelector(".loader")
+      setTimeout(() => { loaderVar.style.display="none"; 
+      displayOutput();
+  
+  }, 1000);
+  }
 
 
+  btnSubmitVar=document.querySelector('#submitdate');
+  birthdayVar=document.querySelector('#birthday');
+  btnSubmitVar.addEventListener("click",CallCheckPalindrome);
+  btnSubmitVar.addEventListener("click",ShowLoader);
+   function CallCheckPalindrome(){
 
-if(found==1)
+
+  console.log(birthdayVar.value)
+  birthdayVarDate = new Date(birthdayVar.value)
+  console.log(birthdayVarDate.getDate());
+  console.log((birthdayVarDate.getMonth())+1);
+  console.log(birthdayVarDate.getFullYear());
+
+  var date = {
+    day: birthdayVarDate.getDate(),
+    month: (birthdayVarDate.getMonth())+1,
+    year: birthdayVarDate.getFullYear()
+  }
+
+IsPalindrome();
+  if(found==1)
 {
     hideLoader();
     console.log("Hurray, Your birthday is Palindrome");
     showOutputMsg="Hurray, Your birthday is Palindrome";
-    //document.querySelector('.display').innerHTML = showOutputMsg
+    //document.querySelector('.display').innerHTML = showOutputMsg;
+    displayOutput();
+    found=0;
     
 }
 else{
+    output=getNextPalindromeDate(date);
+    num_of_days=output[0];
+    next_palindrome_date=''+output[1].day+'-'+output[1].month+'-'+output[1].year
+    
+    num_of_days=output[0];
+        next_palindrome_date=''+output[1].day+'-'+output[1].month+'-'+output[1].year
     hideLoader();
-    console.log("Sorry, Your birthday is not palindrome. You missed it by "+ min_value + " days. The nearest Palindrome date is "+Palindromes[min_index]);
-    showOutputMsg= "Sorry, Your birthday is not palindrome. You missed it by "+ min_value + " days. The nearest Palindrome date is "+Palindromes[min_index]
+    console.log("Sorry, Your birthday is not palindrome. You missed it by "+ num_of_days + " days. The nearest Palindrome date is "+next_palindrome_date);
+    showOutputMsg= "Sorry, Your birthday is not palindrome. You missed it by "+ num_of_days + " days. The nearest Palindrome date is "+next_palindrome_date
     //document.querySelector('.display').innerHTML = showOutputMsg
-}
-}
-else{
-    hideLoader()
-    console.log("Please enter a date");
-    showOutputMsg="Please enter a date";
-    //document.querySelector('.display').innerHTML = showOutputMsg;
+    displayOutput();
 }
 
 }
 function displayOutput(){
     document.querySelector('.display').innerHTML = showOutputMsg;
-}
-function ShowLoader()
-{
-    loaderVar=document.querySelector(".loader")
-    setTimeout(() => { loaderVar.style.display="block";
-    document.querySelector('.display').innerHTML = ''; }, 0);
-}
-function hideLoader()
-{
-    loaderVar=document.querySelector(".loader")
-    setTimeout(() => { loaderVar.style.display="none"; 
-    displayOutput();
-
-}, 1000);
 }
